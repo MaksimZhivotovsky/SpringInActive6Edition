6d92efa9d0fce5912be13b5a99418bb0890ceec5
maksi <maksi@DESKTOP-9Q8R6C6>
2022-12-10 10:51:17 +0500

    init master

---
+++ SpringInActive6Edition/src/main/java/com/example/max/SpringInActive6EditionApplication.java
@@ -0,0 +1,13 @@
+package com.example.max;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInActive6EditionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInActive6EditionApplication.class, args);
+	}
+
+}
---
+++ SpringInActive6Edition/src/test/java/com/example/max/SpringInActive6EditionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.max;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInActive6EditionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
